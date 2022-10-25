4752ccafe3f79fdfc77e82b3dea10c8bcb6f725a
Roland <r.bockholt@student.tudelft.nl>
2022-10-25 17:35:02 +0200

    Add barycentric and compute interpolation
    COmpute barycentric coordinates and compute interpolated normal

--- src/interpolate.cpp
+++ src/interpolate.cpp
@@ -3,14 +3,19 @@
 
 glm::vec3 computeBarycentricCoord (const glm::vec3& v0, const glm::vec3& v1, const glm::vec3& v2, const glm::vec3& p)
 {
-    // TODO: implement this function.
-    return glm::vec3(0.0);
+    float detT = (v1.y - v2.y) * (v0.x - v2.x) + (v2.x - v1.x) * (v0.y - v2.y);
+    float one = (v1.y - v2.y) * (p.x - v2.x) + (v2.x - v1.x) * (p.y - v2.y);
+    float two = (v2.y - v0.y) * (p.x - v2.x) + (v0.x - v2.x) * (p.y - v2.y);
+    float u = one / detT;
+    float v = two / detT;
+    float w = 1 - u - v;
+    return glm::vec3(u,v,w);
 }
 
 glm::vec3 interpolateNormal (const glm::vec3& n0, const glm::vec3& n1, const glm::vec3& n2, const glm::vec3 barycentricCoord)
 {
-    // TODO: implement this function.
-    return glm::vec3(0.0);
+    glm::vec3 iNormal = barycentricCoord.x * n0 + barycentricCoord.y * n1 + barycentricCoord.z * n2;
+    return iNormal;
 }
 
 glm::vec2 interpolateTexCoord (const glm::vec2& t0, const glm::vec2& t1, const glm::vec2& t2, const glm::vec3 barycentricCoord)
