9cb9437464f87c5babae249b1b3c358ed0c24a2e
Sem Hennekam <s.hennekam@student.tudelft.nl>
2022-11-02 12:04:46 +0100

    Remove comment

--- src/bounding_volume_hierarchy.cpp
+++ src/bounding_volume_hierarchy.cpp
@@ -54,11 +54,6 @@ std::vector<std::vector<float>> findBounds(Scene* pScene, std::vector<int> &inde
         }
     }
     return bounds;
-
-    /* Remaining problem with this function: it iterates over the indexes (=2*n_triangles) instead of the vertices, even though the 
-    *  vertices decide the final x,y,z values of the AABB, wasting time because the amount of triangles is larger than the amount of
-    *  vertices. (and we check way more vertices than needed, because triangles may point to the same vertices.)
-    */
 }
 
 /* rootNodeHelper - Helper function to create the root node.
